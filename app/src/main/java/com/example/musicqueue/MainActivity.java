3ec08cc3997c43fc1d874eb05ae220da44a2b278
hoangtuyb96 <hoangtuyb96@gmail.com>
2018-07-25 13:21:26 +0700

    build_list_music

--- app/src/main/java/com/example/musicqueue/MainActivity.java
+++ app/src/main/java/com/example/musicqueue/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.musicqueue;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerViewSongs;
+    private SongAdapter mSongAdapter;
+    private ArrayList<Song> mSongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerViewSongs = findViewById(R.id.rv_songs);
+        mSongs = new ArrayList<>();
+        mSongAdapter = new SongAdapter(this, getSongList());
+        mRecyclerViewSongs.setAdapter(mSongAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        mRecyclerViewSongs.setLayoutManager(linearLayoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public ArrayList<Song> getSongList() {
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                mSongs.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+        }
+        Log.e("Number of Songs", ""+mSongs.size());
+        return mSongs;
     }
 }
---
+++ app/src/main/java/com/example/musicqueue/SongAdapter.java
@@ -0,0 +1,57 @@
+package com.example.musicqueue;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
+    private Context mContext;
+    private List<Song> mSongs;
+    private LayoutInflater mLayoutInflater;
+
+    public SongAdapter(Context mContext,List<Song> mSongs) {
+        this.mContext = mContext;
+        this.mSongs = mSongs;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public SongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.row_song, parent, false);
+        return new SongViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
+        holder.bindData(mSongs.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSongs.size();
+    }
+
+    public class SongViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mArtistTextView;
+
+        public SongViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mTitleTextView = itemView.findViewById(R.id.text_title);
+            mArtistTextView = itemView.findViewById(R.id.text_artist);
+        }
+
+        public void bindData(Song song) {
+            mTitleTextView.setText(song.getTitle());
+            mArtistTextView.setText(song.getArtist());
+        }
+    }
+}
